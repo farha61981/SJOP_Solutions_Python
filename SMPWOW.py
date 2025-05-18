b7a40865ec9aeb98a31cd24d38a0e96afcfa860c
Farha Kousar <147942855+farha61981@users.noreply.github.com>
2025-05-18 17:27:51 +0530

    Create SMPWOW.py

---
+++ SMPWOW.py
@@ -0,0 +1,11 @@
+'''SMPWOW - Wow
+#basics
+
+Input
+Given a positive integer 0 < x < 50.
+
+Output
+Print one word: Wo...ow (letter o must be repeated x times).'''
+
+x = int(input())
+print("W" + "o" * x + "w")
